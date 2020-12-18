f777e2641c6f998ef2e3850f93f8b9fd03b0e9f8
zeinjendeya <z.jendeya@gmail.com>
2020-12-18 16:14:12 +0200

    server app/listen

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('port', 3000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+module.exports = app;
