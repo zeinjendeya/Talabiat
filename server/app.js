de1d9fa7dcc6ccda58734b19b2f3e7be165af941
zeinjendeya <z.jendeya@gmail.com>
2020-12-25 19:11:10 +0200

    fixed issue

--- server/app.js
+++ server/app.js
@@ -18,7 +18,12 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   verifyToken(req.cookies.token)
     .then(() => {
+      if(req.originalUrl === "/login.html" || req.originalUrl === "/"){
+        res.redirect("/restaurants.html")
+      }else{
+      // if the page request is login or signup goto restaurant
       express.static(join(__dirname,"..", "static", "private"))(req, res, next);
+    }
     })
     .catch(() => {
       express.static(join(__dirname, "..","static", "public"))(req, res, next);
