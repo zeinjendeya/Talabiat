d32ec484216039dc12fbab40c8fdb9bf051b2c3f
zeinjendeya <z.jendeya@gmail.com>
2020-12-27 16:28:46 +0200

    hopefully fixed

--- server/app.js
+++ server/app.js
@@ -2,36 +2,38 @@ const express = require("express");
 const { join } = require("path");
 
 const cookieParser = require("cookie-parser");
+require("env2")("./config.env");
 
 const router = require("./router");
 const { verifyToken } = require("./src/utils/index");
 
 const app = express();
 
-app.set("port", 5000);
+app.set("port", process.env.PORT);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 app.use((req, res, next) => {
   verifyToken(req.cookies.token)
     .then(() => {
-      if(req.originalUrl === "/login.html" || req.originalUrl === "/"){
-        res.redirect("/restaurants.html")
-      }else{
-      // if the page request is login or signup goto restaurant
-      express.static(join(__dirname,"..", "static", "private"))(req, res, next);
-    }
+      if (req.originalUrl === "/login.html" || req.originalUrl === "/") {
+        res.redirect("/restaurants.html");
+      } else {
+        // if the page request is login or signup goto restaurant
+        express.static(join(__dirname, "..", "static", "private"))(
+          req,
+          res,
+          next
+        );
+      }
     })
     .catch(() => {
-      express.static(join(__dirname, "..","static", "public"))(req, res, next);
+      express.static(join(__dirname, "..", "static", "public"))(req, res, next);
     });
 });
 
-app.use(express.static(join(__dirname, "..","static", "general")));
-
+app.use(express.static(join(__dirname, "..", "static", "general")));
 
 app.use(router);
 
